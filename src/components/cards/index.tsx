7719de7f72e771f8812503b24e9ee42038300bfa
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2025-06-28 01:13:25 -0300

    feat: adiciona Componente Tag

---
+++ src/components/cards/index.tsx
@@ -0,0 +1,38 @@
+import './style.css'
+import Button from "../CardButton/index";
+import Tag from '../CardTag';
+
+interface CardProps {
+  date: string;
+  title: string;
+  name: string;
+  technologies: string[];
+  description: string;
+  image: string
+  link: string;
+}
+
+function Card({ name, technologies, description, image, link }: CardProps) {
+  return (
+    <div className='card-container'>
+      <div className="">
+        <div className="card-image">
+          <img src={`img/${image}`} />
+          <Button text="VER" href={link} />
+        </div>
+
+        <div className="card-info">
+          <h3>{name}</h3>
+          <div>
+            {technologies.map((tech, index) => (
+              <Tag key={index} name={tech} />
+            ))}
+          </div>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
