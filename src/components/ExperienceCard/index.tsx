72bed84d02ccf67ce81846793c7e8d5cca563a7c
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2025-06-29 22:34:44 -0300

    feat: Finalizada base da seção Experiência

---
+++ src/components/ExperienceCard/index.tsx
@@ -0,0 +1,34 @@
+import "./style.css";
+
+interface ExperienceCardProps {
+  title: string;
+  place: string;
+  duration: string;
+  image: string;
+  paragraphs: string[];
+}
+
+function ExperienceCard({ title, place, duration, image, paragraphs }: ExperienceCardProps) {
+  return (
+    <div className="experience-card-container">
+      <div className="experience-card">
+        <div className="top-row">
+          <img src={image} alt={`${place} logo`} className="experience-img" />
+          <div className="experience-header">
+            <h3>{title}</h3>
+            <p><strong>{place}</strong></p>
+            <p><strong>{duration}</strong></p>
+          </div>
+        </div>
+
+        <div className="bottom-content">
+          {paragraphs.map((text, index) => (
+            <p key={index}>- {text}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExperienceCard;
--- src/sections/experience/index.tsx
+++ src/sections/experience/index.tsx
@@ -1,6 +1,7 @@
 import './style.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import SectionTitle from '../../components/SectionTitle';
+import ExperienceCard from '../../components/ExperienceCard';
 
 function Experience() {
 
@@ -9,23 +10,47 @@ function Experience() {
       place: 'IFSC Garopaba',
       title: "Monitor de Matemática",
       duration: "08/2023-12/2023",
-      paragraph: [
-        'Algoritmos e Lógica de Programação',
-        'Banco de Dados',
-        'Empreendedorismo',
-        'Inglês Aplicado',
+      paragraphs: [
+        'Como monitor de matemática no IFSC, assumi dezenas de responsabilidades para apoiar o aprendizado dos mais de 25 alunos que me procuraram, fortalecendo o ambiente educacional, colaborando com a professora da disciplina para garantir a eficácia das estratégias de ensino.',
+        'Colaborei na criação de apostilas, desenvolvendo recursos didáticos que auxiliaram na compreensão dos conteúdos de matemática.',
+        'Desempenhei um papel essencial na comunicação efetiva com as turmas, garantindo a clareza nas informações relativas aos conteúdos e atividades programadas.',
+        'Ministrei quase 60 aulas de reforço, proporcionando suporte aos alunos em diversos tópicos de matemática.',
       ],
-      image: "/src/assets/img/faculmatch.png",
+      image: "/src/assets/img/experience-dg.png",
+    },
+    {
+      place: 'IFSC Garopaba',
+      title: "Monitor de Matemática",
+      duration: "08/2023-12/2023",
+      paragraphs: [
+        'Como monitor de matemática no IFSC, assumi dezenas de responsabilidades para apoiar o aprendizado dos mais de 25 alunos que me procuraram, fortalecendo o ambiente educacional, colaborando com a professora da disciplina para garantir a eficácia das estratégias de ensino.',
+        'Colaborei na criação de apostilas, desenvolvendo recursos didáticos que auxiliaram na compreensão dos conteúdos de matemática.',
+        'Desempenhei um papel essencial na comunicação efetiva com as turmas, garantindo a clareza nas informações relativas aos conteúdos e atividades programadas.',
+        'Ministrei quase 60 aulas de reforço, proporcionando suporte aos alunos em diversos tópicos de matemática.',
+      ],
+      image: "/src/assets/img/experience-dg.png",
+    },
+    {
+      place: 'IFSC Garopaba',
+      title: "Monitor de Matemática",
+      duration: "08/2023-12/2023",
+      paragraphs: [
+        'Como monitor de matemática no IFSC, assumi dezenas de responsabilidades para apoiar o aprendizado dos mais de 25 alunos que me procuraram, fortalecendo o ambiente educacional, colaborando com a professora da disciplina para garantir a eficácia das estratégias de ensino.',
+        'Colaborei na criação de apostilas, desenvolvendo recursos didáticos que auxiliaram na compreensão dos conteúdos de matemática.',
+        'Desempenhei um papel essencial na comunicação efetiva com as turmas, garantindo a clareza nas informações relativas aos conteúdos e atividades programadas.',
+        'Ministrei quase 60 aulas de reforço, proporcionando suporte aos alunos em diversos tópicos de matemática.',
+      ],
+      image: "/src/assets/img/experience-dg.png",
     },
   ]
 
   return (
     <div className="experience" id="experiencia">
       <SectionTitle title={"EXPERIÊNCIA"} positioning='mid-right'/>
-      <div className="formations">
+      <div className="experiences-div">
           {cardsData.map((card, index) => (
-            <div key={index}>
-              {/* <ExpansiveCard {...card} /> */}
+            <div className='experience-div' key={index}>
+              <ExperienceCard {...card} />
             </div>
           ))}
       </div>
