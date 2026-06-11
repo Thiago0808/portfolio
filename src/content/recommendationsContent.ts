e6f777987c04db6b3d5836ba3f8b1808242ff0e5
Thiago0808 <thiagoliveeeira@gmail.com>
2026-06-11 14:49:07 -0300

    feat: Adiciona recomendação de Miriam

--- src/content/recommendationsContent.ts
+++ src/content/recommendationsContent.ts
@@ -1,8 +1,8 @@
-import faculmatchImg from "../assets/img/projects/faculmatch.png";
 import alineImg from "../assets/img/recommendations/aline.jpg";
 import emmanuelImg from "../assets/img/recommendations/emmanuel.png";
 import leandroImg from "../assets/img/recommendations/leandro.jpg";
 import brunoImg from "../assets/img/recommendations/bruno.jpg";
+import miriamImg from "../assets/img/recommendations/miriam.jpg";
 
 export interface Recommendation {
   id: string;
@@ -35,5 +35,11 @@ export const recommendationsContent: Recommendation[] = [
     name: "Bruno da Silva",
     company: "Central Contabilidade Ltda",
     photo: brunoImg,
+  },
+  {
+    id: "rec5",
+    name: "Miriam Narvaz",
+    company: "Instituto Federal de Santa Catarina",
+    photo: miriamImg,
   },
 ];
